6b06bff10867c69a3824e15638a59a9b74066be9
NatarazNarendra <narendraz88912@gmail.com>
2023-03-07 23:33:41 +0530

    changes for controller&junit

--- src/main/java/in/niraj/spring/springbootjpah2/controller/CustomerController.java
+++ src/main/java/in/niraj/spring/springbootjpah2/controller/CustomerController.java
@@ -1,18 +1,19 @@
 package in.niraj.spring.springbootjpah2.controller;
-
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import in.niraj.spring.springbootjpah2.controller.exceptions.CustomerNotFoundException;
 import in.niraj.spring.springbootjpah2.controller.exceptions.ErrorObject;
@@ -22,21 +23,36 @@ import in.niraj.spring.springbootjpah2.repository.ICustomerRepo;
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
-	// private final Logger log = (Logger)
-	// LoggerFactory.getLogger(CustomerController.class);
+//	 private final Logger log = (Logger)
+	//LoggerFactory.getLogger(CustomerController.class.getName());
 	@Autowired
 	ICustomerRepo customerRepo;
+//Postmapping approach :1
+	/*
+	 * @PostMapping(value = "/customers") // @ReteLimiter(name="emailratelimiter")
+	 * // @RateLimiter("R") public ResponseEntity<Customers>
+	 * saveCustomer(@RequestBody Customers customer) {
+	 * log.info("customer",customer); try { return new
+	 * ResponseEntity<>(customerRepo.save(customer), HttpStatus.CREATED); } catch
+	 * (Exception e) { return new
+	 * ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); }
+	 
+	}*/
+//Postmapping approach :2	
+	 @PostMapping
+	    @ResponseStatus(HttpStatus.CREATED)
+	  //  @ApiOperation(value = "Execute POST method")
+	    public ResponseEntity<Customers> createNewUser_whenPostUser(@RequestBody Customers user) {
 
-	@PostMapping(value = "/customers")
-	// @ReteLimiter(name="emailratelimiter")
-	// @RateLimiter("R")
-	public ResponseEntity<Customers> saveCustomer(@RequestBody Customers customer) {
-		try {
-			return new ResponseEntity<>(customerRepo.save(customer), HttpStatus.CREATED);
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+		 Customers createdUser = customerRepo.save(user);
+
+	        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+	                .path("/{id}")
+	                .buildAndExpand(createdUser.getId())
+	                .toUri();
+
+	        return ResponseEntity.created(uri).body(createdUser);
+	    }
 
 	@GetMapping("/customers")
 	public ResponseEntity<List<Customers>> getAllCustomers() {
@@ -81,4 +97,7 @@ public class CustomerController {
 	 * return new ResponseEntity<>(tutorial, HttpStatus.OK); }
 	 */
 
+	
+	
+	
 }
--- src/test/java/in/niraj/spring/springbootjpah2/controller/CustomerControllerTest.java
+++ src/test/java/in/niraj/spring/springbootjpah2/controller/CustomerControllerTest.java
@@ -1,8 +1,12 @@
 package in.niraj.spring.springbootjpah2.controller;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
@@ -15,11 +19,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import in.niraj.spring.springbootjpah2.entity.Customers;
 import in.niraj.spring.springbootjpah2.repository.ICustomerRepo;
+import in.niraj.spring.springbootjpah2.util.JsonUtil;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CustomerController.class)
@@ -48,4 +54,13 @@ public class CustomerControllerTest {
 		when(customerRepo.findById(1l)).thenReturn(Optional.empty());
 		mockMvc.perform(get("/api/customerss/{id}", id)).andDo(print()).andExpect(status().isNotFound());
 	}
+	
+	/*
+	 * @Test public void createUser_whenPostMethod() throws Exception { Customers
+	 * user = new Customers((long) 1, "narendra","Bombay");
+	 * given(customerRepo.save(user)).willReturn(user); user.setName("narendra");
+	 * mockMvc.perform(post("/api") // .contentType(MediaType.APPLICATION_JSON)
+	 * .content(JsonUtil.toJson(user))) .andExpect(status().isCreated()); //
+	 * .andExpect(jsonPath("$.name", is(user.getName()))); }
+	 */
 }
